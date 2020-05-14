51855cd3552378d571a0a69fca4afdb8b0381635
Marcela Maciel <fmbergamo@gmail.com>
2020-05-14 09:53:39 -0300

    Finalizacao Codigo APITest

--- routes/api.php
+++ routes/api.php
@@ -19,34 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('articles', 'ArticleController@index', function() {
-        // If the Content-Type and Accept headers are set to 'application/json', 
-        // this will return a JSON structure. This will be cleaned up later.
-        return Article::all();
-    });
-     
-    Route::get('articles/{id}', 'ArticleController@show', function($id) {
-        return Article::find($id);
-    });
-
-    Route::post('articles', 'ArticleController@store', function(Request $request) {
-        return Article::create($request->all);
-    });
-
-    Route::put('articles/{id}', 'ArticleController@update', function(Request $request, $id) {
-        $article = Article::findOrFail($id);
-        $article->update($request->all());
-
-        return $article;
-    });
-
-    Route::delete('articles/{id}', 'ArticleController@delete', function($id) {
-        Article::find($id)->delete();
-
-        return 204;
-    });
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
 });
-
-Route::post('register', 'Auth\RegisterController@register');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout');
